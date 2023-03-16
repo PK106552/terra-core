7f27ee63f127acba8e4b1674466a72c7d9d0195c
Pravinkumar K S <pk106552@cerner.net>
2023-03-16 17:01:58 +0530

    Fixed WDIO issue

--- packages/terra-terra-test-comp/tests/wdio/terra-test-comp-spec.js
+++ packages/terra-terra-test-comp/tests/wdio/terra-test-comp-spec.js
@@ -1,9 +1,9 @@
 Terra.describeViewports('TerraTestComp', ['tiny', 'medium', 'large'], () => {
   describe('Default', () => {
-    before(() => browser.url('/raw/tests/terra-terra-test-comp/terra-test-comp/default-terra-test-comp'));
+    before(() => browser.url('/components/terra-terra-test-comp/terra-test-comp/terra-test-comp'));
 
     it('validates the element', () => {
-      Terra.validates.element();
+      Terra.validates.element('fusion');
     });
   });
 });
